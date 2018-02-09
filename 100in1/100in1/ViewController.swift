44f9d8568a73f03cb1a20cd41d3bfd80409c4b59
Inmay <aokatta@163.com>
2018-02-09 15:46:58 +0800

    new

---
+++ 100in1/100in1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  100in1
+//
+//  Created by WuYueFeng on 2018/2/9.
+//  Copyright © 2018年 WuYueFeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
